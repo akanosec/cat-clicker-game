13a08cb14633ebc8a2d976aa485a0d6c67df4f63
akano <akano@uwu.black>
2025-02-26 17:23:39 -0800

    Update base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cat-clicker-game/', // replace with your repo name
+  base: '/', // replace with your repo name
 })
